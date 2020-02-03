cef66ed896a91d6a18e524d80ab166400bb69fad
Votre Nom <Vous@exemple.com>
2020-02-03 20:50:00 +0100

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  qrData:null
+  createdCode:null
+  scannedCode:String
+  constructor(private barcodescanner : BarcodeScanner,public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  createCode(){
+    this.createdCode = this.qrData;
+  }
+  scanCode(){
+    this.barcodescanner.scan().then(barcodeData =>{
+      this.scannedCode = barcodeData.text;
+    })
   }
 
 }
